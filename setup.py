018a67ce133aed61167a84e6662c4b47634abe4e
pavel-paulau <pavel.paulau@gmail.com>
2013-05-17 16:00:37 +0300

    create logger package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='logger',
+    version='1.1',
+    description='Python logging helper',
+    author='Pavel Paulau',
+    author_email='pavel.paulau@gmail.com',
+    packages=['logger'],
+    package_data={'logger': ['logging.conf']},
+)
