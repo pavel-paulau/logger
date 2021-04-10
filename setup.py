6342396d424e3c3b42e3e8d0f4a13683853a6c37
Pavel <pavel.paulau@gmail.com>
2021-04-10 16:25:07 -0700

    Specify package license for pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='logger',
-    version='1.4',
+    version='1.5',
     description='Python logging helper',
     author='Pavel Paulau',
     author_email='pavel.paulau@gmail.com',
+    license='Apache License 2.0',
     packages=['logger'],
     package_data={'logger': ['logging.conf']},
 )
